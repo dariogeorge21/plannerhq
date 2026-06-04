4c50350f522d4236fcd0d641bb66db9b4f338ce9
Dario George <edu.dariogeorge21@gmail.com>
2026-06-04 11:12:52 +0530

    shadcn ui init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [require("tailwindcss-animate")],
+}
